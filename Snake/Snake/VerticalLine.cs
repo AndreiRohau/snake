ae0be37ecba0399e7669f6f76711f219ff4081e8
andreirohau <rohau.andrei@gmail.com>
2016-06-19 16:40:47 +0300

    same

--- Snake/Snake/VerticalLine.cs
+++ Snake/Snake/VerticalLine.cs
@@ -10,10 +10,10 @@ namespace Snake
     {
         List<Point> pList;
 
-        public VerticalLine(int yTop, int yBottom, int x, char sym)
+        public VerticalLine(int yUp, int yDown, int x, char sym)
         {
             pList = new List<Point>();
-            for (int y = yTop; y <= yBottom; y++)
+            for (int y = yUp; y <= yDown; y++)
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
