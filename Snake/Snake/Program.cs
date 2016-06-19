330edb1f57d1ed0ab6f570e41ec220a5f9dc1163
andreirohau <rohau.andrei@gmail.com>
2016-06-19 22:31:32 +0300

    SNAKE IN THE GARDEN

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -24,6 +24,7 @@ namespace Snake
             //Отрисовка точек
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake( p, 4, Direction.RIGHT);
+            snake.Drow();
 
 
             Console.ReadLine();
