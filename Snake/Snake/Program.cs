385457ba417a6e716bda9309d92fe3d8b523c862
andreirohau <rohau.andrei@gmail.com>
2016-06-19 22:17:34 +0300

    timely

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -22,8 +22,8 @@ namespace Snake
 
 
             //Отрисовка точек
-            //Point p = new Point(4, 5, '*');
-            //Snake snake = new Snake( p, 4, Direction.RIGHT);
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake( p, 4, Direction.RIGHT);
 
 
             Console.ReadLine();
--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -8,9 +8,14 @@ namespace Snake
 {
     class Snake : Figure
     {
-        public Snake(Point tail, int length, int direction)
+        public Snake(Point tail, int length, Direction direction)
         {
-
+            for(int i = 0; i < length; i++)
+            {
+                Point p = new Point(tail);
+                p.Move(i, direction);
+                pList.Add(p);
+            }
         }
     }
 }
